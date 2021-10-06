85ad994271be8981c0df612ab82d7533a928c539
Sharon Segal <sharon.j.segal@gmail.com>
2021-10-06 13:42:34 +0300

    hotfix: printjs

--- utils/constants.js
+++ utils/constants.js
@@ -18,60 +18,51 @@ export const kitCardArray = [
     ],
   },
   {
-    type: '_type-science',
+    type: '_type-art',
     image: {
-      src: './images/science_icon.svg',
-      alt: 'science icon of a vial',
+      src: './images/art_icon.svg',
+      alt: 'Icon of a color pallete',
     },
-    title: 'Science Kit',
+    title: 'Art Kit',
     description:
-      'Kids will learn to focus and to solve a problem. This Kit get them into basics of Astronomy, Math, Physics and Chemistry.',
+      'his Kit introduces them into the wonderful world of arts. They will learn to draw, paint and even sculpt!',
     list: [
-      'Scientific journal to read and fill',
-      'Chemical experiments',
-      'Water tubes maze',
-      'DIY slime ingredients',
-      'Robotics task',
-      'Draw the planet map',
+      'Color pencils',
+      'Origami paper and instructions',
+      'New Stickers every month',
+      'Playdough!',
     ],
   },
   {
-    type: '_type-science',
+    type: '_type-nature',
     image: {
-      src: './images/science_icon.svg',
-      alt: 'science icon of a vial',
+      src: './images/nature_icon.svg',
+      alt: 'icon of a leaf for the nature kit',
     },
-    title: 'Science Kit',
+    title: 'Nature Kit',
     description:
-      'Kids will learn to focus and to solve a problem. This Kit get them into basics of Astronomy, Math, Physics and Chemistry.',
+      'Learn about the natural world, from the tiny bacteria swimming in a puddle to the huge elephant stomping in the jungle!',
     list: [
-      'Scientific journal to read and fill',
-      'Chemical experiments',
-      'Water tubes maze',
-      'DIY slime ingredients',
-      'Robotics task',
-      'Draw the planet map',
+      'A monthly nature subject to tackle',
+      'Different maps of the world',
+      'Flower seeds you can plant and grow',
+      'Educational games with a nature theme',
     ],
   },
   {
-    type: '_type-science',
+    type: '_type-surprise',
     image: {
-      src: './images/science_icon.svg',
-      alt: 'science icon of a vial',
+      src: './images/surprise_icon.svg',
+      alt: 'Icon of a magical boat',
     },
-    title: 'Science Kit',
+    title: 'Mystery Kit',
     description:
-      'Kids will learn to focus and to solve a problem. This Kit get them into basics of Astronomy, Math, Physics and Chemistry.',
+      "Every month your child will get a surprise kit, it could be science, arts or nature. It's mystery! who doesn't like a nice surprise?",
     list: [
-      'Scientific journal to read and fill',
-      'Chemical experiments',
-      'Water tubes maze',
-      'DIY slime ingredients',
-      'Robotics task',
-      'Draw the planet map',
+      'A random monthly kit!',
+      "It could be the science kit this month but shh! it's a secret!",
     ],
   },
 ];
 
-export const printfile =
-  'https://code.s3.yandex.net/web-developer/project-1/sprint-1-en-brief.pdf';
+export const printfile = '../freebies/ladybug.pdf';
